8ca90cc307277c175fbde0dc8e4e24a553707d0e
marcelo <mzumbin@gmail.com>
2019-05-12 18:41:12 -0300

    Initial Commit

---
+++ ray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ray
+//
+//  Created by Marcelo de Mattos Nascimento on 12/05/19.
+//  Copyright © 2019 Marcelo de Mattos Nascimento. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
