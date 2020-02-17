b976ef3492be29d8234e7972df872ad8953671c5
marcelo <mzumbin@gmail.com>
2020-02-16 22:12:35 -0300

    new files

--- ray/main.cpp
+++ ray/main.cpp
@@ -7,9 +7,64 @@
 //
 
 #include <iostream>
+#include "ray.h"
+#include <CImg.h>
+#include "sphere.h"
+#include "hittable_list.h"
 
+
+using namespace cimg_library;
+
+
+float hit_sphere(const Vector3f& center, float radius, const ray& r) {
+    Vector3f oc = r.origin() - center;
+    float a = r.direction().dot( r.direction());
+    float b = 2.0 * oc.dot( r.direction());
+    float c = oc.dot( oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant) ) / (2.0*a);
+    }
+}
+
+Vector3f color(const ray& r) {
+    float t = hit_sphere(Vector3f(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        auto N = (r.point_at_parameter(t) - Vector3f(0,0,-1)).normalized();
+        return 0.5*Vector3f(N.x()+1, N.y()+1, N.z()+1);
+    }
+    auto unit_direction = (r.direction()).normalized();
+    t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*Vector3f(1.0, 1.0, 1.0) + t*Vector3f(0.5, 0.7, 1.0);
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int nx = 200;
+    int ny = 100;
+    CImg<unsigned char> visu(nx,ny,1,3,0);
+    
+    Vector3f higher_left_corner(-2.0, 1.0, -1.0);
+    Vector3f horizontal(4.0, 0.0, 0.0);
+    Vector3f vertical(0.0, -2.0, 0.0);
+    Vector3f origin(0.0, 0.0, 0.0);
+    cimg_forXY(visu,i,j) {
+        
+   double u = double(i) / double(nx);
+   double v = double(j) / double(ny);
+   ray r(origin, higher_left_corner + u*horizontal + v*vertical);
+   Vector3f col = color(r);
+   int ir = int(255.99*col.x());
+   int ig = int(255.99*col.y());
+   int ib = int(255.99*col.z());
+
+        visu(i,j) = ir;
+        visu(i,j,0,1) =ig;
+        visu(i,j,0,2)=ib;
+    }
+   
+    visu.save("/Users/marcelo/Downloads/t.bmp");
     return 0;
+    
 }
---
+++ ray/ray_trace.cpp
@@ -0,0 +1,136 @@
+/*****************************
+ ray_trace.c
+
+ This file contains skeleton code for the ray tracing
+ assignment.  You need to write the code to ray trace
+ the scene read in from the input file.
+*******************************/
+
+#include "ray_trace.h"
+#include "read_input.h"
+////extern void readSceneFile(char *filename);
+//
+///* the information read from the file */
+////SceneInfo scene;
+//
+///* keep low while testing, raise to see high quality image */
+//int win_size = 490;
+//
+///* to hold the color information */
+//float **rbitmap, **gbitmap, **bbitmap;
+//
+///*
+//*  rayTrace
+//*  This is the function that raytraces the given scene.  It
+//*  should calculate the color value of each pixel and draw
+//*  it in the interface window.
+//*/
+//
+//void rayTrace(void)
+//{
+//	int i, j;
+//	float rcolor = 0.0;
+//	float gcolor = 0.0;
+//	float bcolor = 0.0;
+//    glClearColor(0.0, 0.0, 0.0, 0.0);
+//	glClear(GL_COLOR_BUFFER_BIT);
+//
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    // allocate memory for our local buffer
+//	rbitmap = (float **)malloc(sizeof(float *) * win_size);
+//	gbitmap = (float **)malloc(sizeof(float *) * win_size);
+//	bbitmap = (float **)malloc(sizeof(float *) * win_size);
+//
+//	for(i=0; i<win_size; i++){
+//		// print statement to help you keep track of progress
+//		if(i % 10 == 0)
+//			printf("Processing scanline %d\n", i);
+//
+//		// REMOVE THIS LINE! THIS IS ONLY TO HELP YOU ENSURE THAT SOMETHING IS BEING DRAWN!!
+//		rcolor += 0.005;
+//
+//		// allocate memory for this row in our local buffer
+//		rbitmap[i] = (float *)malloc(sizeof(float) * win_size);
+//		gbitmap[i] = (float *)malloc(sizeof(float) * win_size);
+//		bbitmap[i] = (float *)malloc(sizeof(float) * win_size);
+//
+//		for(j=0; j<win_size; j++){
+//
+//			// determine the world coordinate location of the pixel (i,j)
+//			// and create the primary ray
+//
+//			// for each object, determine if the primary ray intersects
+//			// the object; keep track of the nearest intersected object
+//
+//			// determine if the intersection point is in shadow for each
+//			// of the light sources
+//
+//			// determine the illumination at the intersection point based
+//			// on unoccluded light sources and ambient lighting
+//
+//			// set the color for the pixel in our local buffer
+//			rbitmap[i][j] = rcolor;
+//			gbitmap[i][j] = gcolor;
+//			bbitmap[i][j] = bcolor;
+//		}
+//	}
+//
+//	// Now scale all the color values (if necessary)
+//	for(i=0; i<win_size; i++){
+//		for(j=0; j<win_size; j++){
+//
+//		}
+//	}
+//
+//	// Now draw all the pixels
+//	glBegin(GL_POINTS);
+//	for(i=0; i<win_size; i++){
+//		for(j=0; j<win_size; j++){
+//			glColor3f(rbitmap[i][j], gbitmap[i][j], bbitmap[i][j]);
+//			glVertex2i(i, j);
+//		}
+//	}
+//	glEnd();
+//	glFlush();
+//}
+//
+//void changeSize(int w, int h) {
+//
+//    // Prevent a divide by zero, when window is too short
+//    // (you cant make a window of zero width).
+//    //win_size=w;
+//    //gluOrtho2D(0.0, win_size, 0.0, win_size);
+//    //win_size = 500-1;
+//    //gluOrtho2D(0.0, win_size, 0.0, win_size);
+//   glutReshapeWindow(win_size, win_size);
+//}
+//
+///*
+//*	Main function.  Reads in the scene information,
+//*	places it in data structures, and creates an interface window.
+//*/
+//
+//int main0(int argc, char **argv)
+//{
+//	/* initialize */
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+//
+//	/* read input */
+//	readSceneFile("/Users/marcelo/Downloads/ray_trace/scene1.txt");
+//
+//	/* create the interface window */
+//
+//    glutInitWindowSize(win_size, win_size);
+//	glutCreateWindow("Ray Tracing");
+//	gluOrtho2D(0.0, win_size, 0.0, win_size);
+//    //glutReshapeWindow(win_size, win_size);
+//	/* set the display function */
+//	glutDisplayFunc(rayTrace);
+//   // glutReshapeFunc(changeSize);
+//    //glutIdleFunc(rayTrace);
+//    /* enter the main loop */
+//	glutMainLoop();
+//    return 0;
+//}
+
